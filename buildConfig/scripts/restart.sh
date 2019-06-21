efcba823e38e5d890306026bb9730f686d3d4a5b
liyifei <18813752467@163.com>
2019-06-21 18:17:30 +0800

    删除jvm参数

--- buildConfig/scripts/restart.sh
+++ buildConfig/scripts/restart.sh
@@ -10,7 +10,7 @@ JAR_PID=$(eval ${JAR_PID_CMD})
 if [[ -n "$JAR_PID" ]]; then
   sudo kill -9 $JAR_PID
 fi
-sudo nohup java -Xbootclasspath/a:./resources -Xmx128m -Xms128m -Xmn50m -Xss128k -XX:ParallelGCThreads=2 -Djava.compiler=NONE -jar ${PROJECT_NAME}.jar spring &>> /data/logs/soar/${PROJECT_NAME}.log &
+sudo nohup java -Xbootclasspath/a:./resources -jar ${PROJECT_NAME}.jar spring &>> /data/logs/soar/${PROJECT_NAME}.log &
 sleep 10s
 tail -50 /data/logs/soar/${PROJECT_NAME}.log
 
